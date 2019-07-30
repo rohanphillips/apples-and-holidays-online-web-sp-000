01aae989a335d3ff20b468e881180d3a96c32b61
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-30 19:46:06 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,26 +18,45 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_supplies[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash.each do |holiday, season|
+    if holiday == :winter
+      season.each do |holiday_name, data|
+        #binding.pry
+        data << supply
+      end
+    end
+  end
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
-
+  holiday_hash.each do |holiday, season|
+    season.each do |holiday_name, data|
+      if holiday_name == :memorial_day
+        data << supply
+      end
+    end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
-
+  holiday_hash.each do |holiday_season, holidayname|
+    binding.pry
+    #if holiday_season.has_value?(season) == false
+    #  holiday_season.store(holiday_name, supply_array)
+    #end
+  end
 end
 
 def all_winter_holiday_supplies(holiday_hash)
@@ -61,10 +80,3 @@ def all_holidays_with_bbq(holiday_hash)
   # include the string "BBQ"
 
 end
-
-
-
-
-
-
-
